d69c75ec2ae6bed36124b5d1d6e7928dfea19728
melina <boukharimelina44@gmail.com>
2026-03-24 12:15:03 +0100

    ajout des fonctions check résolution du probléme d'indexation

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,24 +4,24 @@ export let weaponList: any[] = [];
 export abstract class Weapon {
     name: string;
     damage: number;
-    hits : number;
-    description : string;
-    constructor(name: string, damage: number , hits : number) {
+    hits: number;
+    description: string;
+    constructor(name: string, damage: number, hits: number) {
         this.name = name;
         this.damage = damage;
         this.hits = hits;
         this.description = ""
-        const describe = weapon_desc.find(d => d.name === name);
+        const describe = weapon_desc.find(d => d.name === this.name);
         if (describe) {
-            this.description = describe.description;       
-         }
+            this.description = describe.description;
+        }
     }
 
     getDamage(): number {
-        if(this.hits === 1) {
-        return this.damage ;
-    }   
-    return (this.damage * (Math.floor(Math.random() * this.hits)));
+        if (this.hits === 1) {
+            return this.damage;
+        }
+        return (this.damage * (Math.floor(Math.random() * this.hits)));
     }
 
 }
@@ -61,7 +61,7 @@ class Crossbow extends Weapon {
     constructor() {
         super('crossbow', 2, 5);
     }
-}       
+}
 class Darts extends Weapon {
     constructor() {
         super('darts', 1, 3);
@@ -71,7 +71,7 @@ class Dagger extends Weapon {
     constructor() {
         super('dagger', 3, 1);
     }
-}   
+}
 
 
 export let weapons = [new Chatchet(), new Knife(), new Spear(), new Sword(), new Halberd(), new Bow(), new Crossbow(), new Darts(), new Dagger()];
@@ -110,82 +110,89 @@ export function init() {
 
 export function newRound(hasInit: boolean) {
 
-    if(!hasInit) { throw new Error('Game not initialized'); }
-        return {
-            playerWeapon: weaponList[Math.floor(Math.random() * weaponList.length)],
-            enemyWeapon: null,
-            hasRound: true,
-            hasFought: false
-        }
-    } 
+    if (!hasInit) { throw new Error('Game not initialized'); }
+    return {
+        playerWeapon: weaponList[Math.floor(Math.random() * weaponList.length)],
+        enemyWeapon: null,
+        hasRound: true,
+        hasFought: false
+    }
+}
 
-export function fight(playerHealth: number, enemyHealth: number, playerWeapon: any, hasInit: boolean, hasRound: boolean, hasFought: boolean): Array<number|boolean> {
-    
-    if(!hasInit){
-        
+export function fight(playerHealth: number, enemyHealth: number, playerWeapon: any, hasInit: boolean, hasRound: boolean, hasFought: boolean): Array<number | boolean> {
+
+    if (!hasInit) {
+
         throw new Error('Game not initialized');
     }
-        if(!hasRound){ 
-            throw new Error('Round not initialized');
-        
+
+    if (!hasRound) {
+        throw new Error('Round not initialized');
+
     }
-            if(hasFought) { 
-                throw new Error('Round already played');
-            }
+    if (hasFought) {
+        throw new Error('Round already played');
+    }
+
+    let playerDamages: number = 0;
+    let enemyDamages: number = 0;
+
+    if (playerWeapon === null) {
+        throw new Error('Invalid weapon');
+    }
+
+    playerDamages += playerWeapon.getDamage();
+
+    let enemyWeapon = weaponList[Math.floor(Math.random() * weaponList.length)];
+
+    if (enemyWeapon === null) {
+        throw new Error('Invalid weapon');
+    }
+    enemyDamages += enemyWeapon.getDamage();
+
+    if (playerDamages === enemyDamages) {
+        return [playerHealth, enemyHealth ,enemyWeapon, true, false, false];
+    }
+
+    if (playerDamages < enemyDamages) {
+
+        playerHealth -= enemyDamages - playerDamages;
+    }
+
+    if (playerDamages > enemyDamages) {
+        enemyHealth -= playerDamages - enemyDamages;
+    }
+
+    playerHealth = checkHealthPositive(playerHealth);
+
+    enemyHealth = checkHealthPositive(enemyHealth);
+
+    let playerWon = checkPlayerWon(playerHealth, enemyHealth);
+
+    let playerLost = checkPlayerLost(playerHealth, enemyHealth);
     
-                let playerDamages: number = 0;
-                let enemyDamages: number = 0;
-                if(playerWeapon === null) {
-                    throw new Error('Invalid weapon');
-                }
-                playerDamages += playerWeapon.getDamage();
-                        
-                let enemyWeapon = weaponList[Math.floor(Math.random() * weaponList.length)];
-                
-                if(enemyWeapon === null) {
-                    throw new Error('Invalid weapon');
-                }
-                enemyDamages += enemyWeapon.getDamage();
-
-                if(playerDamages === enemyDamages) {
-                    return [playerHealth, enemyHealth];
-                }
-                
-                if(playerDamages < enemyDamages) {
-                    
-                    playerHealth -= enemyDamages - playerDamages ;                
-                }
-                
-                if(playerDamages > enemyDamages) {
-                    enemyHealth -=  playerDamages - enemyDamages ;
-                }
-           
-               playerHealth = checkHealthPositive(playerHealth);
-            
-               enemyHealth = checkHealthPositive(enemyHealth);
-                
-
-                // check if the game is over and the player has won
-                if(enemyHealth === 0) {
-                    return [playerHealth, enemyHealth, enemyWeapon, true, true, false];
-                }
-
-
-                // check if the game is over and the player has lost
-                if(playerHealth === 0) {
-                    return [playerHealth, enemyHealth, enemyWeapon, true, false, true];
-                }
-            
-                return [playerHealth, enemyHealth, enemyWeapon, true, false, false];
-            }
-        
+    return [playerHealth, enemyHealth, enemyWeapon, true, playerWon, playerLost];
+}
 
 
 
-function checkHealthPositive(health : number){
-    if(health <= 0) {
+function checkHealthPositive(health: number) {
+    if (health <= 0) {
         return 0;
     }
     return health;
 }
 
+function checkPlayerWon(playerHealth: number, enemyHealth: number) {
+    if (enemyHealth === 0) {
+        return true;
+    }
+    return false;
+}
+
+function checkPlayerLost(playerHealth: number, enemyHealth: number) {   
+  if(playerHealth === 0) {
+    return true;
+  }
+  return false;
+}
